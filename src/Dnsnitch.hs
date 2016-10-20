07df1fbe296e1b3f4fccd54c2c48fc1a22ee9da6
Ossi Herrala <oherrala@iki.fi>
2016-10-20 14:32:33 +0300

    Drop DNS cache time to 20 seconds

--- src/Dnsnitch.hs
+++ src/Dnsnitch.hs
@@ -10,7 +10,7 @@ import qualified Dnsnitch.HTTP      as HTTP
 main :: Int -> Int -> IO ()
 main dnsPort httpPort = do
   -- Cache with key-value expiration after N seconds
-  cache <- Cache.newCache 60
+  cache <- Cache.newCache 20
 
   _ <- forkIO $ DNS.dnsMain dnsPort cache
   HTTP.httpMain httpPort cache
